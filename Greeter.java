30af353a6ae96649ee97a033537185c47f8d1265
Nicole <nicole.omuoyo@strathmore.edu>
2025-04-29 12:42:08 +0300

    challenge 1

---
+++ Greeter.java
@@ -0,0 +1,9 @@
+public class Greeter {
+    void hello(){
+        System.out.println("Hello there");
+    }
+    public static void main(String[]args){
+        Greeter hello=new Greeter();
+        hello.hello();
+    }
+}
